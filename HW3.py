b6afa044c63a384f345d8b2153ebb17a5fffe6b1
mashihill <keeneking@gmail.com>
2015-04-16 00:25:35 +0800

    wrong...

--- HW3.py
+++ HW3.py
@@ -4,15 +4,35 @@
 from __future__ import division
 from math import *
 import scipy.stats
+import datetime
+
+def tradingDuration(startDate, endDate):
+
+    startDate = [ int(x) for x in startDate.split('-') ]
+    endDate = [ int(x) for x in endDate.split('-') ]
+    t0 = datetime.date(*startDate)
+    t1 = datetime.date(*endDate)
+    daygenerator = (t0 + datetime.timedelta(x + 1) for x in xrange((t1 - t0).days))
+    duration = sum(1 for day in daygenerator if day.weekday() < 5) 
+    duration += 1
+
+    return duration
 
 
 def BOPF(data):
 
+    totalTradingDays = tradingDuration(data['T0'], data['T1'])
+    exercisableDays = tradingDuration(data['T2'], data['T3'])
+    yearTradingDays = tradingDuration(data['T0'][0:5]+'01-01', data['T0'][0:5]+'12-31')
+
+    print 'hi', tradingDuration('2015-04-15', '2015-04-16')
+
     # Initial value
     S = data['S']
     X = data['X']
-    t = data['t']
-    n = data['n']
+    m = data['m']
+    t = float(totalTradingDays) / yearTradingDays
+    n = totalTradingDays * m 
     s = data['s'] / 100  # convert from percentage to decimal
     r = data['r'] / 100
     u = exp(s * sqrt(t / n))
@@ -37,28 +57,38 @@ def BOPF(data):
     EuroCall = S * CallSum1 - X * exp(-r_ * n) * CallSum2
     EuroPut = X * exp(-r_ * n) * PutSum2 - S * PutSum1
 
+
     # America put
     # Initialize Value at time t
-    ValueFlow = [max(X - (S * (u ** (n-i)) * (d ** i)), 0) for i in range(n+1)]
+    PutValueFlow = [max(X - (S * (u ** (n-i)) * (d ** i)), 0) for i in range(n+1)]
 
+    print 'exercisableDays', exercisableDays
     # Run backward to time 0
+    exercisableDays -= 1
+    count = 0
     for time in reversed(range(n)):
+        print time
+        count += 1
         # Payoff of early exercise
         EarlyExercise = [max(X - (S * (u ** (time-i)) * (d ** i)), 0) for
                          i in range(time+1)]
         # Continuation value
-        ValueFlow = [((p * ValueFlow[i] + (1-p) * ValueFlow[i+1]) / R) for
+        PutValueFlow = [((p * PutValueFlow[i] + (1-p) * PutValueFlow[i+1]) / R) for
                      i in range(time+1)]
+
         # Find the larger value
-        ValueFlow = [max(EarlyExercise[i], ValueFlow[i]) for
-                     i in range(len(ValueFlow))]
+        #if (time in range(n - exercisableDays * m, n) and (time % 2 == 1)):
+        #if (count < exercisableDays * m) and (time % 2 == 0):
+        if (count <= exercisableDays * m) :
+            print 'hi'
+            PutValueFlow = [max(EarlyExercise[i], PutValueFlow[i]) for
+                         i in range(len(PutValueFlow))]
+
 
     # Output Information
 
-    outputs = [('European Call', str(EuroCall)),
-               ('European Put', str(EuroPut)),
-               ('American Call', str(EuroCall)),
-               ('American Put', str(ValueFlow[0]))]
+    outputs = [ ('Bermuda Call', str(EuroCall)),
+               ('Bermuda Put', str(PutValueFlow[0]))]
 
     # Aligned output
     print "S=%r, X=%r, s=%r%%, t=%r, n=%r, r=%r %%:" % (S, X, data['s'],
@@ -78,7 +108,7 @@ if __name__ == '__main__':
             data = json.load(data_file)
         for test in data:
             BOPF(test)
-        print "~~ end ~~"
+        #print "~~ end ~~"
     else:
         print 'This requires an input file.  Please select one from the data \
-               directory. (e.g. python HW2.py ./data)'
+               directory. (e.g. python HW3.py ./data)'
